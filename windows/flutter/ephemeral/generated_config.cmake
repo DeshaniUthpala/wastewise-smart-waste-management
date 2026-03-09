7cae7b4b1d69e582e16d627a70c7f511dc192e9e
DeshaniUthpala <is2021469@mgt.rjt.ac.lk>
2026-03-09 23:29:27 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\flutter\\flutter_windows_3.38.5-stable\\flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Deshani\\Desktop\\final project\\wasteapp" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\flutter\\flutter_windows_3.38.5-stable\\flutter\\flutter"
+  "PROJECT_DIR=C:\\Users\\Deshani\\Desktop\\final project\\wasteapp"
+  "FLUTTER_ROOT=D:\\flutter\\flutter_windows_3.38.5-stable\\flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Deshani\\Desktop\\final project\\wasteapp\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Deshani\\Desktop\\final project\\wasteapp"
+  "FLUTTER_TARGET=C:\\Users\\Deshani\\Desktop\\final project\\wasteapp\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzguNQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049ZjZmZjE1MjlmZA==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049MTUyN2FlMGVjNQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMC40"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Deshani\\Desktop\\final project\\wasteapp\\.dart_tool\\package_config.json"
+)
